6b9e7c1c9853cb8a1a831ff05b1a8cfa3c85fd71
himanshu2168.be21@chitkara.edu.in <himanshu2168.be21@chitkara.edu.in>
2023-11-30 16:16:38 +0530

    final commit for portfolio

---
+++ script.js
@@ -0,0 +1,33 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    sections.forEach(section=>{
+        let top= window.scrollY;
+        let offset = section.offsetTop-150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+        if (top >= offset && top < offset+height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id+']').classList.add('active')
+            })
+        }
+    })
+    /*==================== sticky navbar ====================*/
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+};
+
+
+/*==================== scroll reveal ====================*/
+
+
+/*==================== typed js ====================*/
